9a5a1246ce957674004693557f6d7595f1a5cc7a
peppe-quint-boogaard <peppequintb@gmail.com>
2019-04-25 14:03:02 +0200

    math

--- public/js/index.js
+++ public/js/index.js
@@ -8,54 +8,49 @@ const input = document.querySelector("#m");
     console.log(time);
     const timestamp = document.createElement("h2");
     timestamp.setAttribute("class", "timestamp");
-    timestamp.textContent = "Today at " + time;
+    timestamp.textContent = time;
 
-    document.querySelector("#list").append(timestamp);
+    document.querySelector("header").append(timestamp);
   });
 
   socket.on("traffic distance", meters => {
     console.log(meters);
     const distance = document.createElement("h2");
     distance.setAttribute("class", "distance");
-    distance.textContent = "Total distance is " + meters + " meters";
+    distance.textContent = "Totaal aantal kilometers: " + meters / 1000;
 
-    document.querySelector("#list").append(distance);
+    document.querySelector("header").append(distance);
   });
 
   socket.on("traffic jams", traffic => {
+    console.log(traffic);
     const trafficJam = traffic.map(traffic => {
       console.log(traffic);
       if (traffic.events.traffic.delay !== null) {
         const jam = document.createElement("li");
         if (traffic.road.startsWith("A", 0)) {
-          console.log("A-weg");
           jam.setAttribute("class", "road-information ");
           jam.innerHTML = `
             <span class='road road-red'>${traffic.road}</span>
             <p class='location'>Van ${traffic.events.traffic.from} naar ${
             traffic.events.traffic.to
           }</p>
-            <div class='properties'>
-            <p class='distance'><i class="material-icons md-dark">access_time</i>
-            ${traffic.events.traffic.distance}</p>
-            <p class='delay'><i class="material-icons md-dark">settings_ethernet</i>
-            ${traffic.events.traffic.delay}</p>
-            </div>
+            <p class='distance'><i class="material-icons md-dark">settings_ethernet</i>
+            ${traffic.events.traffic.distance / 1000} kilometer</p>
+            <p class='delay'><i class="material-icons md-dark">access_time</i>
+            ${traffic.events.traffic.delay / 60} minuten</p>
           `;
         } else if (traffic.road.startsWith("N", 0)) {
-          console.log("A-weg");
           jam.setAttribute("class", "road-information ");
           jam.innerHTML = `
             <span class='road road-yellow'>${traffic.road}</span>
             <p class='location'>Van ${traffic.events.traffic.from} naar ${
             traffic.events.traffic.to
           }</p>
-            <div class='properties'>
-            <p class='distance'><i class="material-icons md-dark">access_time</i>
-            ${traffic.events.traffic.distance}</p>
-            <p class='delay'><i class="material-icons md-dark">settings_ethernet</i>
-            ${traffic.events.traffic.delay}</p>
-            </div>
+            <p class='distance'><i class="material-icons md-dark">settings_ethernet</i>
+            ${traffic.events.traffic.distance / 1000} kilometer</p>
+            <p class='delay'><i class="material-icons md-dark">access_time</i>
+            ${traffic.events.traffic.delay / 60} minuten</p>
           `;
         }
         document.querySelector("#list").append(jam);
